4f6495d9054d68363ce10766177769e8e434296b
CAMILO-PC\Pc <camus35@hotmail.com>
2017-03-29 10:00:29 -0500

    se crea componente experimento

---
+++ ng_laboratorio/app/lab/experimento/experimento.componente.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'experimento',
+    moduleId: module.id,
+    templateUrl: 'experimento.component.html'
+})
+export class ExperimentoComponent {
+
+}
---
+++ ng_laboratorio/app/lab/experimento/experimento.detalles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'experimento-detalles',
+    moduleId: module.id,
+    templateUrl: 'experimento.detalles.component.html'
+})
+export class ExperimentoDetallesComponent {
+
+}
