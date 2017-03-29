84729d46d152f131d4ebc2ecb38e5c3ff32aa207
Servio Andrés Pantoja Rosero <Shd_cristo@hotmail.com>
2017-03-28 21:49:40 -0500

    Adelantos panel de proyecto

--- ng_laboratorio/app/lab/lab.module.ts
+++ ng_laboratorio/app/lab/lab.module.ts
@@ -6,6 +6,9 @@ import { ExperimentoService } from "./experimento/experimento.service";
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { ProyectoComponent } from "./proyecto/proyecto.component";
+import { ProyectoAdjuntosComponent } from "./proyecto/proyecto.adjuntos.component";
+import { ProyectoInformacionBasicaComponent } from "./proyecto/proyecto.informacion.basica.component";
+import { ProyectoPanelComponent } from "./proyecto/proyecto.panel.component";
 import { ProyectoDetalleComponent } from "./proyecto/proyecto.detalle.component";
 import { ProyectoListComponent } from "./proyecto/proyecto.list.component";
 import { RouterModule } from "@angular/router";
@@ -23,12 +26,15 @@ import { EquiposService } from "./equipos/equipos.service";
         CommonModule,
         FormsModule,
         RouterModule.forChild([
-            { path: 'proyectos', component: ProyectoComponent },
+            { path: 'proyectos', component: ProyectoPanelComponent },
             { path: 'protocolos', component: ProtocoloComponent }
         ]),
     ],
     declarations: [
         ProyectoComponent,
+        ProyectoInformacionBasicaComponent,
+        ProyectoAdjuntosComponent,
+        ProyectoPanelComponent,
         ProtocoloComponent,
         ProtocoloBuscadorComponent,
         ProtocoloDetalleComponent,
@@ -40,8 +46,11 @@ import { EquiposService } from "./equipos/equipos.service";
     ],
     exports:[
         NotificacionesComponent,
+        ProyectoInformacionBasicaComponent,
         ProtocoloBuscadorComponent,
         ProtocoloDetalleComponent,
+        ProyectoAdjuntosComponent,
+        ProyectoPanelComponent,
         ProyectoListComponent,
         ProyectoDetalleComponent,
         EquiposComponent
---
+++ ng_laboratorio/app/lab/proyecto/proyecto.informacion.basica.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: "proyecto-informacion-basica",
+    moduleId: module.id,
+    templateUrl: 'proyecto.informacion.basica.component.html',
+})
+export class ProyectoInformacionBasicaComponent {
+
+}
