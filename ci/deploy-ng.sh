675741f2f96d8d24375b029f9e3417773b7f8fb1
Felipe Martinez <f.martinez2@uniandes.edu.co>
2017-04-13 17:32:36 -0500

    archivo para hacer cambios y lanzar el build en trigger

--- ci/deploy-ng.sh
+++ ci/deploy-ng.sh
@@ -1,5 +1,5 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git remote add origin-ci https://femago:$GITHUB_API_KEY@github.com/UAMISO4101/BrigadaAgilLab.git
+git remote add origin-ci https://$GITHUB_PUSH_USER:$GITHUB_API_KEY@github.com/UAMISO4101/BrigadaAgilLab.git
 git subtree push --prefix ng origin-ci deploy-ng > /dev/null
