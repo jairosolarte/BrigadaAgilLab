912d253c573354858e5934f35489455db526474f
Felipe Martinez <f.martinez2@uniandes.edu.co>
2017-04-13 17:22:47 -0500

    redireccion salida git subtree

--- ci/deploy-ng.sh
+++ ci/deploy-ng.sh
@@ -2,4 +2,4 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
 git remote add origin-ci https://femago:$GITHUB_API_KEY@github.com/UAMISO4101/BrigadaAgilLab.git
-git subtree push --prefix ng origin-ci deploy-ng
+git subtree push --prefix ng origin-ci deploy-ng > /dev/null
