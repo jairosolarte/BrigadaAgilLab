cd2a0a51c341cf0a2930717a26cc6237702fd9b2
Servio Andrés Pantoja Rosero <Shd_cristo@hotmail.com>
2017-04-16 23:25:59 -0500

    Adelantos de buscados de protocolos - web service y frontend- falta consumir el servicio

---
+++ src/app/lab/protocolo/service/protocolo.list.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by alejandroquintero on 23/03/17.
+ */
+
+import {Protocolo} from "./protocolo";
+
+//storing array of data in Fruits
+export const PROTOCOLOS: Protocolo[] =[
+   {id: 1, nombre :"Generación de emitopaticos", fecha:"23 Ene 2017",version:"1"},
+   {id: 2, nombre :"Valoración desinfectantes 2 ", fecha:"23 Ene 2017",version:"2"}
+];
---
+++ src/app/lab/protocolo/service/protocolo.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by alejandroquintero on 23/03/17.
+ */
+export interface Protocolo{
+   id: number;
+   nombre: string;
+   fecha: string;
+   version: string;
+}
