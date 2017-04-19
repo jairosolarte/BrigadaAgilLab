5dbd10fcf90f45868737a9ba2c94f96fc06cdd72
Felipe Martinez <f.martinez2@uniandes.edu.co>
2017-04-18 21:44:18 -0500

    Ajustes json proyecto

--- src/app/lab/proyecto/service/proyecto.service.ts
+++ src/app/lab/proyecto/service/proyecto.service.ts
@@ -8,20 +8,19 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProyectoService {
-    private url_servicios_proyectos = environment.url_servicios + "proyecto/";
+  private url_servicios_proyectos = environment.url_servicios + "proyecto/";
 
-    constructor(private _http: Http) {
-    }
+  constructor(private _http: Http) {
+  }
 
-    getProyectos(): Observable<Proyecto[]> {
-        return this._http.get(this.url_servicios_proyectos).map((response: Response) => <Proyecto[]>response.json());
-    }
+  getProyectos(): Observable<Proyecto[]> {
+    return this._http.get(this.url_servicios_proyectos).map((response: Response) => <Proyecto[]>response.json());
+  }
 
-    nuevoProyecto() {
+  nuevoProyecto() {
+  }
 
-    }
+  eliminarProyecto() {
 
-    eliminarProyecto() {
-
-    }
+  }
 }
