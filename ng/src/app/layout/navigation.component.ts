11246a00fbee11ee267aae22e9f79d106830d573
Felipe Martinez <f.martinez2@uniandes.edu.co>
2017-04-23 17:01:28 -0500

    UI - Agregar efectos para marcar el elemento seleccionado del menu

--- ng/src/app/layout/navigation.component.ts
+++ ng/src/app/layout/navigation.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit, ElementRef } from "@angular/core";
+import {Component, OnInit, ElementRef, AfterViewInit} from "@angular/core";
 
-declare var jQuery:any;
+declare var jQuery: any;
 
 @Component({
     selector: 'lab-navigation',
-    moduleId: module.id,
     templateUrl: 'navigation.component.html'
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit, AfterViewInit  {
+
+    ngAfterViewInit(): void {
+    }
+
     constructor(private _el: ElementRef) {
-    
     }
 
     ngOnInit(): void {
-        jQuery(this._el.nativeElement).filter('#side-menu').metisMenu();
-        jQuery(this._el.nativeElement).filter('.sidebar-collapse').slimScroll({
+        let elem = jQuery(this._el.nativeElement);
+        elem.filter('#side-menu').metisMenu();
+        elem.filter('.sidebar-collapse').slimScroll({
             height: '100%',
             railOpacity: 0.9
         });
